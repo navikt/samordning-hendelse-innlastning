99649b14d80c240c3e1652f87a45e641f8cac2b9
Marius Eriksen <marius.eriksen@nav.no>
2019-04-25 09:43:26 +0200

    Bumped junit 5 dependency

--- src/test/java/no/nav/samordning/innlastning/database/DatabaseIntegrationTest.java
+++ src/test/java/no/nav/samordning/innlastning/database/DatabaseIntegrationTest.java
@@ -1,8 +1,10 @@
 package no.nav.samordning.innlastning.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import no.nav.samordning.innlastning.DatabaseTestUtils;
+import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
 import no.nav.vault.jdbc.hikaricp.VaultError;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -15,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DatabaseTest {
 
     private static final String VEDTAK_ID = "JKL678";
@@ -22,9 +25,7 @@ class DatabaseTest {
     private static final String YTELSES_TYPE = "AAP";
     private static final String FOM = "2000-01-01";
     private static final String TOM = "2010-02-04";
-    private static final String ROLE = "some_role";
-    private static final String MOUNT_PATH = "mount/path";
-    private static final String BAD_JDBC_URL = "badUrl";
+
     private static Database database;
 
     @Container
@@ -33,26 +34,20 @@ class DatabaseTest {
     @Container
     private static GenericContainer vaultContainer = setUpVaultContainer();
 
+
     @BeforeAll
-    static void setup() throws Exception {
+    static void setUp() throws Exception {
         runVaultContainerCommands(vaultContainer, postgresqlContainer.getContainerIpAddress());
-        database = new Database(
-                postgresqlContainer.getJdbcUrl(),
-                DB_MOUNT_PATH,
-                DB_ROLE
-        );
+        HikariConfig datasourceConfig = DatasourceConfig.getDatasourceConfig(postgresqlContainer.getJdbcUrl());
+        HikariDataSource datasourceWithVaultIntegration = HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(datasourceConfig, DB_MOUNT_PATH, DB_ROLE);
+        database = new Database(datasourceWithVaultIntegration);
     }
 
     @Test
+    @Order(1)
     void hendelse_inserted_to_db_as_json() throws Exception {
 
-        Hendelse hendelse = new Hendelse();
-        hendelse.setVedtakId(VEDTAK_ID);
-        hendelse.setIdentifikator(IDENTIFIKATOR);
-        hendelse.setYtelsesType(YTELSES_TYPE);
-        hendelse.setFom(FOM);
-        hendelse.setTom(TOM);
-
+        Hendelse hendelse = getTestHendelse();
         database.insert(hendelse);
 
         String expectedHendelse = "{" +
@@ -70,10 +65,29 @@ class DatabaseTest {
     }
 
     @Test
-    void database_throws_VaultError_when_initialized_with_bad_environment_variables() {
+    @Order(2)
+    void insert_fails_when_database_is_down() {
+
+        breakDbConnection();
+
         assertThrows(
-                VaultError.class,
-                () -> new Database(BAD_JDBC_URL, MOUNT_PATH, ROLE)
+                FailedInsert.class,
+                () -> database.insert(getTestHendelse())
         );
+    }
+
+    private void breakDbConnection() {
+        postgresqlContainer.stop();
+        vaultContainer.stop();
+    }
+
+    private Hendelse getTestHendelse() {
+        Hendelse hendelse = new Hendelse();
+        hendelse.setVedtakId(VEDTAK_ID);
+        hendelse.setIdentifikator(IDENTIFIKATOR);
+        hendelse.setYtelsesType(YTELSES_TYPE);
+        hendelse.setFom(FOM);
+        hendelse.setTom(TOM);
+        return hendelse;
     }
 }
