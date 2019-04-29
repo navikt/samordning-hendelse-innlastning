b5aef86eb65ef2662360c4f8612e9dac7f483145
Andreas Skomedal <andreas.skomedal@nav.no>
2019-04-29 10:49:28 +0200

    SAM-147 Fjernet jsonb

--- src/main/java/no/nav/samordning/innlastning/database/Database.java
+++ src/main/java/no/nav/samordning/innlastning/database/Database.java
@@ -1,39 +1,44 @@
 package no.nav.samordning.innlastning.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.sql.DataSource;
 import org.postgresql.util.PGobject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import javax.sql.DataSource;
-import java.sql.*;
 
 public class Database {
 
     private static final Logger LOG = LoggerFactory.getLogger(Database.class);
 
-    private final String INSERT_RECORD_SQL = "INSERT INTO HENDELSER(HENDELSE_DATA) VALUES(to_json(?::json))";
+    private static final String INSERT_RECORD_SQL = "INSERT INTO HENDELSER(HENDELSE_DATA) VALUES(to_json(?::json))";
+    private final ObjectMapper objectMapper;
 
     private DataSource dataSource;
 
     public Database(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.objectMapper = new ObjectMapper();
     }
 
     public void insert(Hendelse hendelse) {
-        Jsonb jsonb = JsonbBuilder.create();
+        String hendelseString = hendelse.toString();
         PGobject pGobject = new PGobject();
         pGobject.setType("jsonb");
 
-        try(Connection connection = dataSource.getConnection()) {
-            pGobject.setValue(jsonb.toJson(hendelse));
+        try (Connection connection = dataSource.getConnection()) {
+            pGobject.setValue(objectMapper.writeValueAsString(hendelse));
             PreparedStatement insertStatement = connection.prepareStatement(INSERT_RECORD_SQL);
             insertStatement.setObject(1, pGobject, Types.OTHER);
             insertStatement.executeUpdate();
-            LOG.info("Inserted: " + hendelse.toString());
-        } catch (SQLException e) {
-            throw new FailedInsert(hendelse.toString(), e);
+            LOG.info("Inserted: {}", hendelseString);
+        } catch (SQLException | JsonProcessingException e) {
+            throw new FailedInsert(hendelseString, e);
         }
     }
 }
