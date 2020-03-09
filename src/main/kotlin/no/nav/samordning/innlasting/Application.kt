2c8d2df099a608196b949587054ee5f7395a3244
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2020-03-09 10:00:35 +0100

    Bumped hendelse kafka topic to v2.

--- src/main/kotlin/no/nav/samordning/innlasting/Application.kt
+++ src/main/kotlin/no/nav/samordning/innlasting/Application.kt
@@ -2,45 +2,47 @@ package no.nav.samordning.innlasting
 
 import no.nav.opptjening.nais.NaisHttpServer
 import no.nav.samordning.innlasting.database.Database
-import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.KafkaStreams.State.*
 import org.slf4j.LoggerFactory
 import javax.sql.DataSource
 import kotlin.system.exitProcess
 
 
 internal class Application(applicationDataSource: ApplicationDataSource, kafkaConfiguration: KafkaConfiguration) {
-    private val hendelseStream: KafkaStreams
+    private val hendelseStream = SamordningHendelseStream.build(
+            kafkaConfiguration.streamConfiguration(),
+            Database(applicationDataSource.dataSource())
+    ).apply {
+        setUncaughtExceptionHandler { t, e ->
+            LOG.error("Uncaught exception in thread {}, closing samordningHendelseStream", t, e)
+            close()
+        }
+    }
+
     @Volatile
     private var shutdown = false
 
     private val isRunning: Boolean
         get() = hendelseStream.state().isRunning
 
-    init {
-        val streamProperties = kafkaConfiguration.streamConfiguration()
-        val database = Database(applicationDataSource.dataSource())
-        val naisHttpServer = NaisHttpServer(::isRunning, "TRUE"::toBoolean)
+
+    private val naisHttpServer = NaisHttpServer(::isRunning, "TRUE"::toBoolean).apply {
         try {
-            naisHttpServer.start()
+            start()
         } catch (e: Exception) {
             LOG.error("NaisServer failed to start", e)
             exitProcess(1)
         }
-
-        hendelseStream = SamordningHendelseStream.build(streamProperties, database)
-        setUncaughtStreamExceptionHandler()
-        Runtime.getRuntime().addShutdownHook(Thread(Runnable(::shutdown)))
     }
 
-    private fun setUncaughtStreamExceptionHandler() = hendelseStream.setUncaughtExceptionHandler { t, e ->
-        LOG.error("Uncaught exception in thread {}, closing samordningHendelseStream", t, e)
-        hendelseStream.close()
+    init {
+        Runtime.getRuntime().addShutdownHook(Thread(Runnable(::shutdown)))
     }
 
     fun run() {
         hendelseStream.setStateListener { newState, oldState ->
             LOG.debug("State change from {} to {}", oldState, newState)
-            if (oldState == KafkaStreams.State.PENDING_SHUTDOWN && newState == KafkaStreams.State.NOT_RUNNING || oldState.isRunning && newState == KafkaStreams.State.ERROR) {
+            if (oldState == PENDING_SHUTDOWN && newState == NOT_RUNNING || oldState.isRunning && newState == ERROR) {
                 LOG.warn("Stream shutdown, stopping nais http server")
                 shutdown()
             }
@@ -51,6 +53,7 @@ internal class Application(applicationDataSource: ApplicationDataSource, kafkaCo
     fun shutdown() = if (!shutdown) {
         shutdown = true
         hendelseStream.close()
+        naisHttpServer.stop()
     } else Unit
 
     interface ApplicationDataSource {
